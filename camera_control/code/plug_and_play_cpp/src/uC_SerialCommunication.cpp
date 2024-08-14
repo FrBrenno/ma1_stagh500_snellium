8c947ebac330345dd51605ceb7a3d32653f3514f
FrBrenno <fr.brenno@gmail.com>
2024-08-14 14:23:32 +0200

    update 14.08

--- camera_control/code/plug_and_play_cpp/src/uC_SerialCommunication.cpp
+++
@@ -1,9 +0,0 @@
-#include "uC_SerialCommunication.hpp"
-
-uC_SerialCommunication::uC_SerialCommunication(){
-    serialName = "Serial Communication";
-}
-
-std::string uC_SerialCommunication::getName() {
-    return serialName;
-}
